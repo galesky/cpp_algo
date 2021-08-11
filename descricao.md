55b9c28421626c8ac0e71a093ec6b23f89fdd3a4
galesky <leonardo.galesky@gmail.com>
2021-08-10 23:44:17 -0300

    add serach methods

---
+++ descricao.md
@@ -0,0 +1,13 @@
+### Objetivo: Aplicar e corroborar conceitos adquiridos com relação à estratégias de busca sobre conjuntos de dados. 
+
+### Tarefa:Implementar três estratégias de busca em um conjunto de inteiros:
+
+a) Busca Binária tradicional. O método deordenação prévia é de escolha do grupo.
+
+b) Busca Linear clássica (a busca é realizada sem preparação inicial do vetor).
+
+c) Busca Linear ordenada. Neste caso o vetor é pré-ordenado e só então as buscas lineares são executadas. A chave é buscada de forma linear  equando o valor presente no vetor é maior que a chave, a busca é encerrada sem sucesso.Entrada: Arquivos em formato txt contendo um primeiro vetor de inteiros que comporá o campo de busca.  Além  disso,  o  arquivo  conterá  um  segundo  vetor  de  inteiros  que  deverão  ser  buscados  no primeiro vetor.Saída: as soluções devem apresentar as posições que os valores buscados (segundo vetor) ocupam no primeiro vetor. Caso um número tenha mais de uma ocorrência, pode-se retornar a primeira vez que ele é encontrado. Caso o número buscado não esteja no vetor, retorna-se o valor -1.
+
+### O que deve ser analisado:
+1) Tempo  cronológico  para  as  duas  estratégias  para  cada  arquivo  de  entrada  (incluindo  método  de ordenação adotado).
+2) Número de operações de comparação entre elementos para as duas estratégias  (incluindo método de ordenação adotado).
--- main.cpp
+++ main.cpp
@@ -4,43 +4,136 @@
 #include <vector>
 #include <sstream>
 #include <iterator>
-
+#include <algorithm>
 #include <stdio.h>
 #include <string.h>
-
 using std::stringstream;
 
-int main() 
-{ 
-    std::ifstream file("./entries/33.txt");
-    std::vector<int> first_line;
-    std::vector<int> second_line;
+int linear_search(std::vector<int> haystack, int needle)
+{
+  for (int i = 0; i < haystack.size(); i++)
+  {
+    if (haystack[i] == needle)
+      return i;
+  }
+  return -1;
+}
 
-    std::string line;
-    std::string strNumInput;
-    bool is_first_line = true;
+// linear search that breaks if if reaches a value on the haystack higher than the target. haystack must be sorted
+int op_linear_search(std::vector<int> haystack, int needle)
+{
+  // greaten than max value
+  if (needle > haystack.back())
+    return -1;
 
-    while (getline(file, line)) {
-        std::string entry;
+  for (int i = 0; i < haystack.size(); i++)
+  {
+    if (haystack[i] == needle)
+      return i;
 
-        if (is_first_line == true) {
-            char* c = const_cast<char*>(line.c_str());
-            char* token = strtok(c, " ");
-            // loop through the string to extract all other tokens
-            while( token != NULL && strcmp(token, "\r") != 0) {
-                first_line.push_back(std::stoi(token));
-                token = strtok(NULL, " ");
-            }
-            is_first_line = false;
-        } else {
-            char* c = const_cast<char*>(line.c_str());
-            char* token = strtok(c, " ");
-            while( token != NULL && strcmp(token, "\r") != 0) {
-                second_line.push_back(std::stoi(token));
-                // printf(" %s\n", token ); //printing each token
-                token = strtok(NULL, " ");
-            }
-        }
+    // if we reached a number greater than the target, the item is not there
+    if (haystack[i] > needle)
+      return -1;
+  }
+  return -1;
+}
+
+int binary_search(std::vector<int> array, int size, int value)
+{
+  int first = 0,       // First array element
+      last = size - 1, // Last array element
+      middle,          // Mid point of search
+      position = -1;   // Position of search value
+  bool found = false;  // Flag
+  while (!found && first <= last)
+  {
+    middle = (first + last) / 2; // Calculate mid point
+    if (array[middle] == value)  // If value is found at mid
+    {
+      found = true;
+      position = middle;
     }
-    printf("Finished!");
+    else if (array[middle] > value) // If value is in lower half
+      last = middle - 1;
+    else
+      first = middle + 1; // If value is in upper half
+  }
+  return position;
+}
+
+int main()
+{
+  std::ifstream file("./entries/3.txt");
+  std::vector<int> first_line;
+  std::vector<int> second_line;
+
+  std::string line;
+  std::string strNumInput;
+  bool is_first_line = true;
+
+  while (getline(file, line))
+  {
+    std::string entry;
+
+    if (is_first_line == true)
+    {
+      char *c = const_cast<char *>(line.c_str());
+      char *token = strtok(c, " ");
+      // loop through the string to extract all other tokens
+      while (token != NULL && strcmp(token, "\r") != 0)
+      {
+        first_line.push_back(std::stoi(token));
+        token = strtok(NULL, " ");
+      }
+      is_first_line = false;
+    }
+    else
+    {
+      char *c = const_cast<char *>(line.c_str());
+      char *token = strtok(c, " ");
+      while (token != NULL && strcmp(token, "\r") != 0)
+      {
+        second_line.push_back(std::stoi(token));
+        // printf(" %s\n", token ); //printing each token
+        token = strtok(NULL, " ");
+      }
+    }
+  }
+
+  // unsorted, linear
+  for (const auto &item : second_line)
+  {
+    int needle_pos = linear_search(first_line, item);
+    std::cout << " " << std::to_string(needle_pos);
+  }
+
+  std::cout << "\n";
+
+  // sort array
+  std::sort(first_line.begin(), first_line.end());
+  // OPTIONAL: remove duplicates
+  first_line.erase(unique(first_line.begin(), first_line.end()), first_line.end());
+
+  // sorted, linear
+  for (const auto &item : first_line)
+  {
+    std::cout << " " << std::to_string(item);
+  }
+
+  std::cout << "\n";
+
+  for (const auto &item : second_line)
+  {
+    int needle_pos = op_linear_search(first_line, item);
+    std::cout << " " << std::to_string(needle_pos);
+  }
+  std::cout << "\n";
+
+  for (const auto &item : second_line)
+  {
+    int needle_pos = binary_search(first_line, first_line.size(), item);
+    std::cout << " " << std::to_string(needle_pos);
+  }
+
+  printf("Finished!");
 }
